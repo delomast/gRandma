cad3ac9a331c53662cef79bc387521d355046798
Tom Delomas <delomast@gmail.com>
2019-12-17 16:39:01 -0700

    in progress, just backing up

--- R/utils.R
+++ R/utils.R
@@ -53,3 +53,31 @@ condProbTrio <- function(ma, pa, kid){
 	return(temp)
 }
 
+#' recode alleles based on key
+#' @param genotypes character vector of genotypes with missing as NA
+#' @param key dataframe with first column being alleles (as characters) and second column being
+#'   integers representing them 
+#' @keywords internal
+#' @noRd
+recodeAlleles <- function(genotypes, key){
+	
+	newGenotypes <- genotypes
+	for(i in 1:nrow(key)){
+		newGenotypes[!is.na(genotypes) & genotypes == key[i,1]] <- key[i,2]
+	}
+	return(as.numeric(newGenotypes))
+}
+
+#' flip alleles represented as integers to have all hets referred to the same (small int first)
+#' @param genotypes dataframe with allele1 in column 1 and allele 2 in column 2, missing as NA
+#' @keywords internal
+#' @noRd
+flipHets <- function(genotypes){
+	
+	newGenotypes <- genotypes
+	boolSelect <- (!is.na(genotypes[,1])) & (genotypes[,1] > genotypes[,2])
+	newGenotypes[boolSelect,1] <- genotypes[boolSelect,2]
+	newGenotypes[boolSelect,2] <- genotypes[boolSelect,1]
+	
+	return(newGenotypes)
+}
